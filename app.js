61d281937a742dbc93a4bde07ecaf2f1cd7457a5
Ami <oparasit38@gmail.com>
2017-04-11 16:22:42 +0700

    home

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var admin = require('./modules/admin');
 var member = require('./modules/member');
 var plan = require('./modules/planning');
 var show = require('./modules/show');
- var path = require('./modules/path');
+// var path = require('./modules/path');
 
 //can recieve api from another domain
 app.use(function(req, res, next) {
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 
 
 app.post(`/show`, (req, res) =>{
-
+ console.log(req.body);
   var showtype = (error, result) => {
   if (error) {
     console.log(error);
@@ -46,15 +46,18 @@ app.post(`/show`, (req, res) =>{
     if(req.body.do == "pc"){
       show.placeCategories(req, showtype);
     }
-    else if(req.body.do == "pp"){
-      show.placePopular(showtype);
-    }
-    else if(req.body.do == "tp"){
-      show.tripsPopular(showtype);
+    else if(req.body.do == "home"){
+    show.home(showtype);
     }
     else if(req.body.do == "tr"){
       show.tripsRecent(showtype);
     }
+    else if(req.body.do == "pp"){
+    show.placePopular(showtype);
+    }
+    else if(req.body.do == "tp"){
+    show.tripsPopular(showtype);
+   }
 
 });
 
--- modules/show.js
+++ modules/show.js
@@ -3,23 +3,86 @@ var assert = require('assert');
 let mongo = require('mongodb').MongoClient;
 let connection = 'mongodb://localhost:27017/bagpaotravel';
 
-// exports.placePopular = function(callback) {
-//     mongo.connect(connection, (err, database) => {
-//       database
-//       .collection('place')
-//       .find({idplace:`${req.body.idplace}`}).count().sort( idplace : -1).toArray(function(err, docs) {
-//   	if (err) {
-//   		callback('cannot connect to database', undefined);
-//   	}else{
-//   		if (docs.length !== 0) {
-//   			callback(undefined, docs);
-//   	}else{
-//   			callback('please input data',undefined);
-//   		}
-//   	}
-//   });
-// });
-// }
+exports.home = function(callback){
+  mongo.connect(connection, (err, database) => {
+    var doc1, doc2;
+    database.collection('place')
+    .find({}).sort({ view : -1}).toArray(function(err, docs) {
+  if (err) {
+    callback('cannot connect to database', undefined);
+    doc1 = docs;
+    next();
+  }else{
+    if (docs.length !== 0) {
+      callback(undefined, docs);
+      doc1 = docs;
+      next();
+  }else{
+      callback('please input data',undefined);
+      doc1 = docs;
+      next();
+    }
+  }
+});
+
+database.collection('trip').aggregate(
+{
+ $project:
+   {
+     name:"$name",
+     creator:"$creator",
+     picture:"$picture",
+     totalAmount: { $sum: [ "$like", "$share" ]}
+   }
+},
+{
+  $limit : 3
+},
+{
+  $sort : {totalAmount : -1}
+}
+).toArray(function(err, docs) {
+if (err) {
+callback('cannot connect to database', undefined);
+doc2 = docs;
+next();
+}else{
+if (docs.length !== 0) {
+  callback(undefined, docs);
+  doc2 = docs;
+  next();
+}else{
+  callback('please input data',undefined);
+  doc2 = docs;
+  next();
+}
+}
+});
+function next(){
+            if(doc1 && doc2){
+                callback(null, [doc1, doc2]);
+            }
+        }
+});
+}
+
+exports.placePopular = function(callback) {
+    mongo.connect(connection, (err, database) => {
+      database
+      .collection('place')
+      .find({}).sort({ view : -1}).toArray(function(err, docs) {
+  	if (err) {
+  		callback('cannot connect to database', undefined);
+  	}else{
+  		if (docs.length !== 0) {
+  			callback(undefined, docs);
+  	}else{
+  			callback('please input data',undefined);
+  		}
+  	}
+  });
+});
+}
 
 exports.placeCategories = function(req, callback) {
    console.log(req.body);
@@ -40,7 +103,6 @@ exports.placeCategories = function(req, callback) {
 });
 }
 
-
 exports.tripsPopular = function(callback) {
     mongo.connect(connection, (err, database) => {
       database
@@ -56,9 +118,11 @@ exports.tripsPopular = function(callback) {
          }
       },
       {
-        $sort : {totalAmount : -1}
+        $limit : 3
       },
-      { $limit : 3 }
+      {
+        $sort : {totalAmount : -1}
+      }
       ).toArray(function(err, docs) {
   	if (err) {
   		callback('cannot connect to database', undefined);
