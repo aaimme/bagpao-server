f92a411953fb7e543013c8b1a1c4e1173c31d368
Ami <oparasit38@gmail.com>
2017-04-26 02:36:58 +0700

    .

--- app.js
+++ app.js
@@ -525,34 +525,34 @@ var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
 });
 
-app.get(`/apidistance`, (req, res) =>{
-  var request = require('request');
-  var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?&origins='+req.query.origin+'&destinations='+req.query.destination+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
-  request(url, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    body = JSON.parse(body);
-//    console.log(body);
-    res.send(body);
-    });
-    });
+// app.get(`/apidistance`, (req, res) =>{
+//   var request = require('request');
+//   var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?&origins='+req.query.origin+'&destinations='+req.query.destination+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
+//   request(url, function (error, response, body) {
+//     console.log('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     // console.log('body:', body); // Print the HTML for the Google homepage.
+//     body = JSON.parse(body);
+// //    console.log(body);
+//     res.send(body);
+//     });
+//     });
 
 
-app.get(`/apigeo`, (req, res) =>{
-  var request = require('request');
-  var latlng = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+req.query.lat+','+req.query.lng+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
-  var address = 'https://maps.googleapis.com/maps/api/geocode/json?address='+req.query.address+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
-  request(latlng, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    body = JSON.parse(body);
-//    console.log(body);
-    res.send(body);
+// app.get(`/apigeo`, (req, res) =>{
+//   var request = require('request');
+//   var latlng = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+req.query.lat+','+req.query.lng+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
+//   var address = 'https://maps.googleapis.com/maps/api/geocode/json?address='+req.query.address+'&key=AIzaSyDGFpo_nftbMYEro-Ff0lkXNdQV7sEwKN8'
+//   request(latlng, function (error, response, body) {
+//     console.log('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     // console.log('body:', body); // Print the HTML for the Google homepage.
+//     body = JSON.parse(body);
+// //    console.log(body);
+//     res.send(body);
 
-    });
-  });
+//     });
+//   });
 
 app.listen(1200, function() {
   console.log('Server running on port 1200...')
