caadc142af027f866e96329825c2f6d8e340b2a0
Ami <m_aime_me@hotmail.com>
2017-05-10 16:04:34 +0700

    new encrypto

--- modules/login.js
+++ modules/login.js
@@ -2,10 +2,24 @@
 var assert = require('assert');
 let mongo = require('mongodb').MongoClient;
 let connection = 'mongodb://localhost:27017/bagpaotravel';
-let crypto  = require('crypto');
 
-function encrypt(password) {
-	return crypto.createHash('sha256').update(password).digest('hex');
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
 }
 
 exports.checkUserLogin = function( req, callback) {
